faa9d3cb7eea67f39cb35a7a0cb282b8f2cc452b
Vaibhav Sharma <vaibhav.sharma@qed42.com>
2021-02-08 17:24:21 +0530

    changed to https endpoint

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@ const Constants = {
   OPEN_WEATHER_API_KEY: 'bf24680bf8e9ae1e83d931ee03535e96',
   OPEN_WEATHER_API: 'https://api.openweathermap.org',
   // BASE_URL: 'http://localhost:8001/api' //local
-  BASE_URL: 'http://13.232.32.117:8001/api' // prod
+  // BASE_URL: 'http://13.232.32.117:8001/api' // prod
+  BASE_URL: 'https://api.abeecee.net/api' // prod
 }
 
 export default Constants;
